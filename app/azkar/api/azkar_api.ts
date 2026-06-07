6372d44eeb0017b6d8ab4995027180d9092ea64f
Omar-Ahmed-Omar <omar.ahmed@mogo-eg.com>
2026-06-08 00:15:14 +0300

    make changes

--- app/azkar/api/azkar_api.ts
+++ app/azkar/api/azkar_api.ts
@@ -1,7 +1,7 @@
 import { azkar } from "@/app/endpoints/endpoint";
-import { env_local } from "@/app/envioroment/enviroment";
+import { env_local, env_prod } from "@/app/envioroment/enviroment";
 import axios from "axios";
 export const getZekr = async (type: string) => {
-  const res = await axios.get(`${env_local}${azkar}/${type}`);
+  const res = await axios.get(`${env_prod}${azkar}/${type}`);
   return res.data;
 };
